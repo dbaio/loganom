acff1dbbc99e4a43fe9178a91795b3e7d7c11b5f
Danilo G. Baio <dbaio@bsd.com.br>
2020-06-23 09:51:24 -0300

    Update to 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Danilo G. Baio'
 author = 'Danilo G. Baio'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
